c19b2f1972ac8256d0c9254e06866ef259117f7f
Hugo Messias <hugomessias9@gmail.com>
2024-12-09 21:48:47 -0300

    Criado arquivo de testes simples para analisar as mudanças feitas

---
+++ test_warez.py
@@ -0,0 +1,10 @@
+from warez import *
+
+
+# filme
+print("MOVIE SOURCES:")
+print(source.warezcdn_servers('tt1187064'))
+
+# serie
+print("SERIES SOURCES:")
+print(source.warezcdn_servers('tt0436992', 1, 1))
